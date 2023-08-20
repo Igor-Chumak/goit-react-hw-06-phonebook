667bef5edc8d19ca51bf6004e82a000ddac483db
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-08-20 13:04:00 +0300

    delete in item

--- src/components/ContactItem/ContactItems.jsx
+++ src/components/ContactItem/ContactItems.jsx
@@ -1,12 +1,16 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import { Delete } from 'components';
 import { ContactItem } from './ContactItems.styled';
+import { deleteContact } from 'store/contactsSlice';
 
 export const ContactItems = ({ name, number, id }) => {
+  const dispatch = useDispatch();
+  const handleDelete = itemId => dispatch(deleteContact(itemId));
   return (
     <ContactItem>
       {name} : {number}
-      <Delete type="button" id={id}>
+      <Delete type="button" id={id} onClick={() => handleDelete(id)}>
         Delete
       </Delete>
     </ContactItem>
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,18 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteContact } from 'store/contactsSlice';
+import { useSelector } from 'react-redux';
 import { getContacts, getFilter } from 'store/selectors';
 import { ContactItems } from 'components';
 import { ContactListBox } from './ContactList.styled';
 
 export const ContactList = () => {
-  const dispatch = useDispatch();
   const filter = useSelector(getFilter);
   const contacts = useSelector(getContacts);
-
-  const handleDelete = itemId => dispatch(deleteContact(itemId));
 
   const contactsToList = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter)
   );
 
   return (
-    <ContactListBox onClick={e => handleDelete(e.target.id)}>
+    <ContactListBox>
       {contactsToList.map(({ id, name, number }) => (
         <ContactItems name={name} number={number} id={id} key={id} />
       ))}
